ebb588184d5c233f9a6c0049638614d00ead7ccf
vrFonics <calebsamek@gmail.com>
2022-11-01 19:30:47 -0400

    Initial Commit

---
+++ Source/ProjectTectonic/ProjectTectonicGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectTectonicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTTECTONIC_API AProjectTectonicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
